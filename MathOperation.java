1e0a4d8af5a7d01cf78179d6639cf3cb5f9ada26
Monika <monikadkale98@gmail.com>
2021-02-24 18:10:11 +0530

    added new program

--- MathOperation.java
+++ MathOperation.java
@@ -2,25 +2,32 @@
 interface IMathFunction {
 
 	int calculate(int a, int b);
+
+        static void PrintResult(int a, int b, String Function, IMathFunction fobj) {
+    	
+		System.out.println("result of " +Function +" is "+fobj.calculate(a, b));
+	    }
 }
 
 public class MathOperation {
 
 		 public static void main(String[] args) {
-		
 		     //method referance	
 		     IMathFunction add = Integer :: sum;
-		     
 		     //lambda function
 		     IMathFunction subtract = (x,y) -> x - y ;
 		     IMathFunction multiply = (x,y) -> x * y ;
 		     IMathFunction div = (x,y) -> x / y ;
-		
-		     
 		     	 System.out.println("addition is " +add.calculate(6,3));
 			 System.out.println("subtraction is " +subtract.calculate(6,3));
 			 System.out.println("multiply is " +multiply.calculate(6,3));
 			 System.out.println("division is " +div.calculate(6,3));
+
+                        //passing lambda expression as function parameter using static function
+			IMathFunction.PrintResult(6, 3, "addition", add);
+		     	IMathFunction.PrintResult(6, 3, "division", subtract);
+		     	IMathFunction.PrintResult(6, 3, "addition", multiply);
+		     	IMathFunction.PrintResult(6, 3, "division", div);
     	 	}
- 
+
 }
